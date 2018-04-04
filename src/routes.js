cf503575834a00a0b1585bdbdf2900ff06f38206
James Irish <james.irish@bcs.org.uk>
2018-04-04 17:50:05 +0100

    added basic auth

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Route, IndexRoute } from "react-router";
 import App from "./components/App";
+import Auth from "./auth";
 import HomePage from "./components/home/HomePage";
 import SignInForm from "./components/admin/SignInForm";
 import RegisterForm from "./components/admin/RegisterForm";
 
+/*eslint-disable react/jsx-no-bind*/
+
+const auth = new Auth();
+
+
+
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={HomePage}/>
-    <Route path="signin" component={SignInForm} />
-    <Route path="register" component={RegisterForm} />
+    <IndexRoute component={(props) => <HomePage auth={auth} {...props} />} />
+    <Route path="signin" component={(props) => <SignInForm auth={auth} {...props} />}  />
+    <Route path="register" component={(props) => <RegisterForm auth={auth} {...props} />}  />
   </Route>
 );
