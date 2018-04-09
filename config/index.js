d5bef5cc961735e625605b9c32ce27a95da1d61c
James Irish <james.irish@bcs.org.uk>
2018-04-09 16:41:10 +0100

    tidy config loading

--- config/index.js
+++ config/index.js
@@ -5,8 +5,6 @@ import nconf from "nconf";
 
 const NODE_ENV = process.env.NODE_ENV;
 
-process.env["mondoDb_url"] = "mongooose";
-
 nconf
   .argv()
   .env(
@@ -20,27 +18,6 @@ nconf
     format: require("nconf-yaml")
   })
   .defaults(yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, "default.yml"), "utf-8")));
-
-/*
-
-
-let processEnvAppKeys = filter(keys(process.env), (key) => key.substr(0, 3) === "app");
-console.log("Env 'app' keys: ", processEnvAppKeys);
-
-let baseConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, "default.yml"), "utf-8"));
-let envConfig = null;
-
-switch (NODE_ENV) {
-  case "production":
-    envConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, "production.yml"), "utf-8"));
-    break;
-  case "staging":
-    envConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, "staging.yml"), "utf-8"));
-    break;
-  default:
-    envConfig = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, "development.yml"), "utf-8"));
-}
-*/
 
 let config = nconf.get();
 
